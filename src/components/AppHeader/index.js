07ca7a322cc8c4bb7b8929c8b021db69bf239339
r-i-c-h <rich@richwerden.com>
2018-11-03 14:32:28 -0700

    Initial Cleanup of Cloned Repo

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -26,15 +26,6 @@ const AppHeader = (props) => {
             href='https://app.netlify.com/start/deploy?repository=https://github.com/netlify/fauna-one-click&stack=fauna'>
             <img src={deployButton} className='deploy-button' alt='deploy to netlify' />
           </a>
-          <div className='view-src'>
-            <a
-              target='_blank'
-              rel='noopener noreferrer'
-              href='https://github.com/netlify/netlify-faunadb-example'>
-              <img className='github-icon' src={github} alt='view repo on github' />
-              View the source Luke
-            </a>
-          </div>
         </div>
       </div>
     </header>

337aafbaa17b5c9faefe9d907e28e0a6e2a8e0e4
r-i-c-h <rich@richwerden.com>
2018-11-03 15:13:20 -0700

    Removes FrontEnd DB call

--- src/App.js
+++ src/App.js
@@ -14,22 +14,7 @@ export default class App extends Component {
     showMenu: false
   }
   componentDidMount() {
-    // Fetch all todos
-    api.readAll().then((todos) => {
-      if (todos.message === 'unauthorized') {
-        if (isLocalHost()) {
-          alert('FaunaDB key is not unauthorized. Make sure you set it in terminal session where you ran `npm start`. Visit http://bit.ly/set-fauna-key for more info')
-        } else {
-          alert('FaunaDB key is not unauthorized. Verify the key `FAUNADB_SERVER_SECRET` set in Netlify enviroment variables is correct')
-        }
-        return false
-      }
-
-      console.log('all todos', todos)
-      this.setState({
-        todos: todos
-      })
-    })
+    // Where it wanted to fetch existing data.
   }
   saveTodo = (e) => {
     e.preventDefault()

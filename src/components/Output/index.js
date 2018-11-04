91716e6b909d7dc97f72a3f37006342e46f10d91
HappyViki <veronica.eulenberg@gmail.com>
2018-11-03 17:58:52 -0700

    Added Output component

---
+++ src/components/Output/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Output.css' // eslint-disable-line
+
+const Output = (props) => {
+  return (
+      <div className="output-container">
+        <div className="output">
+            April has 3 apples, John takes 2 apples, how many apples are there?
+        </div>
+      </div>
+  )
+}
+
+export default Output

34b8edb5423b7437c14b54de53ef06ab25946c78
Veronica Eulenberg <veronica.eulenberg@gmail.com>
2018-11-04 16:37:13 -0800

    form header h2 to h3 and add br

--- src/components/Output/index.js
+++ src/components/Output/index.js
@@ -33,9 +33,10 @@ const Output = props => {
       <div className="output-answer">Answer: 4</div>
 
       <div>
-        <h2>Provide Us Your Email and Feedback</h2>
+        <h3>Provide Us Your Email and Feedback</h3>
         <form method="POST" action="https://formspree.io/lydiarachel@gmail.com">
           <input type="email" className="email" placeholder="Your email" />
+<br>
           <input
             className="text-area"
             name="message"

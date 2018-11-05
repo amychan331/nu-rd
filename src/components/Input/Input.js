555f8067080661aa751d38c679743fd2e893fa66
Veronica Eulenberg <veronica.eulenberg@gmail.com>
2018-11-04 16:26:03 -0800

    fixed button col container thing

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -23,36 +23,42 @@ class Input extends Component {
         <div>
           <h3 htmlFor="comment">Enter Math Equation</h3>
 
-          <form className={"form " + this.props}>
-            <div className="form-inner">
-              <input
-                value={this.state.inputFormula}
-                name="mathInput"
-                onChange={this.handleInputChange}
-                type="text"
-              />
-
-              <button
-                className="btn-1"
-                type="submit"
-                name="action"
-                onClick={this.handleFormSubmit}
-              >
-                <span>Submit</span>
-              </button>
-            </div>
-
-            <div>
-              <button
-                className="btn-2"
-                type="reset"
-                name="action"
-                onClick={this.handleFormSubmit}
-              >
-                <span>Clear</span>
-              </button>
-            </div>
-          </form>
+<form className={"form " + this.props}>
+  <div className="form-inner">
+    <input
+      value={this.state.inputFormula}
+      name="mathInput"
+      onChange={this.handleInputChange}
+      type="text"
+    />
+    <div className="col-container">
+      <div className="col-inner">
+        <button
+          className="btn-1"
+          type="submit"
+          name="action"
+          onClick={this.handleFormSubmit}
+        >
+          <span>Submit</span>
+        </button>
+      </div>
+      <div />
+      <div className="col-inner space" />
+      <div className="col-inner">
+        <div>
+          <button
+            className="btn-2"
+            type="reset"
+            name="action"
+            onClick={this.handleFormSubmit}
+          >
+            <span>Clear</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</form>
 
           <h3 className="gist-comment">Math Word Problem</h3>
           <div>

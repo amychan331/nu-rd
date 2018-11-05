32bd6c2769fd53591dd9ddcca4b270d4c5b479a3
r-i-c-h <rich@richwerden.com>
2018-11-04 19:49:46 -0800

    For some reason Input component didn't update? Fix from curr master

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,13 +1,15 @@
+
 import React, { Component } from "react";
 import "./Input.css";
 
 class Input extends Component {
   state = {
-    inputFormula: "2+2",
+    inputFormula: "",
   };
   handleFormSubmit = event => {
     event.preventDefault();
     this.props.handleFormSubmit(this.state.inputFormula);
+
   };
 
   handleInputChange = event => {
@@ -17,94 +19,54 @@ class Input extends Component {
     });
   };
 
+  handleFormClear = event => {
+    event.preventDefault();
+    this.setState({
+      inputFormula: ""
+    })
+  }
+
   render() {
     return (
       <div>
         <div>
-          <label htmlFor="comment">Enter Math Equation</label>
-      
-<form className={"form " + this.props}>
-  <div className="form-inner">
-    <input
-      value={this.state.inputFormula}
-      name="mathInput"
-      onChange={this.handleInputChange}
-      type="text"
-    />
-    <div className="col-container">
-      <div className="col-inner">
-        <button
-          classname="btn-1"
-          type="submit"
-          name="action"
-          onClick={this.handleFormSubmit}
-        >
-          <span>Submit</span>
-        </button>
-      </div>
-
+          <h3 htmlFor="comment">Enter Math Equation</h3>
 
           <form className={"form " + this.props}>
- 
             <div className="form-inner">
-                <input
-                    value={this.state.inputFormula}
-                    name="mathInput"
-                    onChange={this.handleInputChange}
-                    type="text"
-                />
-           
-              <button className="btn-1"
-                  type="submit"
-                  name="action"
-                  onClick={this.handleFormSubmit}
-                  >
-                    <span>Submit</span>
-                 </button>
-                 </div>
-          
-             <div>
-             <button className="btn-2"
-                 type="reset"
-                 name="action"
-                 onClick={this.handleFormSubmit}
-                 >
-                   <span>Clear</span>
-                 </button>
-                 </div>
+              <input
+                value={this.state.inputFormula}
+                name="mathInput"
+                onChange={this.handleInputChange}
+                type="text"
+              />
 
-              </form>
+              <button
+                className="btn-1"
+                type="submit"
+                name="action"
+                onClick={this.handleFormSubmit}
+              >
+                <span>Submit</span>
+              </button>
+            </div>
 
+            <div>
+              <button
+                className="btn-2"
+                type="reset"
+                name="action"
+                onClick={this.handleFormClear}
+              >
+                <span>Clear</span>
+              </button>
+            </div>
+          </form>
 
-      {/*
-      <div />
-      <div className="col-inner" />
-      <div className="col-inner">
-        <button
-          classname="btn-2"
-          type="reset"
-          name="action"
-          onClick={this.handleFormSubmit}
-        >
-          <span>Clear</span>
-        </button>
-      </div>
-    </div>
-  </div>
-</form>
-*/}
-
-          <h2 className="gist-comment">Math Word Problem</h2>
+          <h3 className="gist-comment">Math Word Problem</h3>
           <div>
             {(this.state.results && this.state.results.name) || ""}
-            {/* {this.props.comments.map(comment => (
-                  <Comment
-                      key={comment._id}
-                      comment={comment.comment}
-                      authorName={comment.author.name}
-                      date = {comment.date}
-                  />
-              ))} */}
+
           </div>
         </div>
       </div>
